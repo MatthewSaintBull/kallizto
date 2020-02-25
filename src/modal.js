1d785e387a5ecea59e93f4fcab997cc5f7e82898
Matteo Santoro <mattinter997@gmail.com>
2020-02-25 09:04:59 +0100

    removed unused src file

--- src/modal.js
+++
@@ -1,8 +0,0 @@
-var modBtns = document.querySelectorAll('.btn-mdl')
-
-modBtns.forEach(modBtn=>{
-    modBtn.addEventListener("click", function(){
-        document.querySelector('.modal').style.display= document.querySelector('.modal').style.display=='block' ? document.querySelector('.modal').style.display='none' : document.querySelector('.modal').style.display='block'
-    })
-})
-
