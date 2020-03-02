365d32c6541569a9a4c4155f3dcc2dde0cb049b8
Matteo  Santoro <sherl@MacBook-Pro-di-Matteo.local>
2020-03-02 18:18:42 +0100

    Implemented Responsive Navbar

--- src/index.js
+++ src/index.js
@@ -3,3 +3,15 @@ document.querySelectorAll('.btn-mdl').forEach(modBtn=>{
         document.querySelector('.modal').style.display= document.querySelector('.modal').style.display=='block' ? document.querySelector('.modal').style.display='none' : document.querySelector('.modal').style.display='block'
     })
 })
+
+const clickedBar = ()=>{
+    var nav = document.querySelector(".navbar-secondary") || document.querySelector(".navbar-primary")
+    nav.querySelectorAll(".btn-primary" || ".btn-secondary").forEach(
+        node=>{
+            node.style.display =  node.style.display == "block" ? "none":"block"
+        }
+    )
+}
+
+document.getElementsByClassName("mobile")[0].addEventListener("click", clickedBar)
+
